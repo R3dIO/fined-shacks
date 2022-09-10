681735a1cfdb2d3a8c53cec86fee0111487b9ba5
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2022-09-10 19:58:52 -0400

    feat: new user details api skeleton

--- web_server/__init__.py
+++ web_server/__init__.py
@@ -7,7 +7,6 @@ from flask import Flask
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from flask_restful import Api
-print(Flask.__dict__)
 
 import firebase_admin
 from firebase_admin import credentials
